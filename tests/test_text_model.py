e222c138551b53081b913a35bf6661a65db78760
Shihabul-Shuvo <shihabul900@gmail.com>
2025-10-03 02:20:32 +0600

    chore: Remove obsolete files including documentation and test scripts

--- tests/test_text_model.py
+++
@@ -1,8 +0,0 @@
-# tests/test_text_model.py
-from transformers import pipeline
-
-
-def test_text_pipeline_run():
-    nlp = pipeline("text-classification", model="tabularisai/multilingual-sentiment-analysis", device=-1)
-    out = nlp("I love this product!")
-    assert isinstance(out, list) and len(out) > 0
